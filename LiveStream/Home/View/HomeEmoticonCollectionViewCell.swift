3d3447c3a73bd92f81bf0d18007e77f9093d862a
duanmingming <geekdmm@163.com>
2018-04-17 16:02:05 +0800

    add emoticon for keyboard

---
+++ LiveStream/Home/View/HomeEmoticonCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeEmoticonCollectionViewCell.swift
+//  LiveStream
+//
+//  Created by GeekDuan on 2018/4/17.
+//  Copyright © 2018年 GeekDuan. All rights reserved.
+//
+
+import UIKit
+
+class HomeEmoticonCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ LiveStream/Home/View/HomeEmoticonView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeEmoticonView.swift
+//  LiveStream
+//
+//  Created by GeekDuan on 2018/4/16.
+//  Copyright © 2018年 GeekDuan. All rights reserved.
+//
+
+import UIKit
+
+class HomeEmoticonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
